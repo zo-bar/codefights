a77b39500c34f0aabd76aa5b19c9400d35d9c592
Zoya <zoya.barabanova@gmail.com>
2017-10-27 22:23:27 -0700

    inerview practice strings

--- regexMatching.java
+++ regexMatching.java
@@ -1,3 +1,6 @@
+/* Given a pattern string and a test string, your task is to implement regex substring matching. If pattern is preceded by a ^, the pattern, excluding the ^, will be matched with the starting position of the test string. If pattern is followed by a $, the pattern, excluding the $, will be matched with the ending position of the test string. If no such markers are present, check whether pattern is a substring of test, regardless of its position.
+*/
+
 boolean regexMatching(String pattern, String test) {
     if (pattern == null || pattern.length() ==0) return true;
     
